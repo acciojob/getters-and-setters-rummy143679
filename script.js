161771b3e23317030d099fd97fcd0fe3c05dea78
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-05-13 19:56:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this.name = name;
+		this.age = age;
+	}
 
-class Student extends Person {}
+	get Name(){
+		return this.name;
+	}
 
-class Teacher extends Person {}
+	set Name(n){
+		this.name = n;
+	}
+}
+
+class Student extends Person {
+	constructor(name, age){
+		super(name, age)
+	}
+
+	void study(){
+		console.log(`${super.name} is studying`)
+	}
+}
+
+class Teacher extends Person {
+	constructor(name, age){
+		super(name, age)
+	}
+
+	void teach(){
+		console.log(`${super.name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
